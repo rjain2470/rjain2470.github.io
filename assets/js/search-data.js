843597ecb59bfc48bf7c9c6bd2b58ae73c01a998
rjain2470 <rjain92682@gmail.com>
2025-12-27 04:56:31 +0000

    Deploying to gh-pages from @ rjain2470/rjain2470.github.io@33ad8a4d1e6f800969ae2664c8c0d06aeb7c5795 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "cv",
-          description: "TBA",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
